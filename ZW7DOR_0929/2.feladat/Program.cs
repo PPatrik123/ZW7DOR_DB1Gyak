40c326c29135149d52d20d0ca19a2c9a0860226d
PPatrik123 <78854607+PPatrik123@users.noreply.github.com>
2021-10-23 17:46:57 +0200

    Add files via upload

---
+++ ZW7DOR_0929/2.feladat/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ZW7DOR_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adatok száma: ");
+            int adatokSzama = Convert.ToInt32(Console.ReadLine());
+            int[] adat = new int[adatokSzama];
+
+            for (int i = 0; i < adatokSzama; i++)
+            {
+                Console.Write($"{i + 1}.adat = ");
+                adat[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Adatok kiírása: Palko.txt állományba");
+            using (FileStream fs = File.Open("Palko.txt", FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    for (int i = 0; i < adatokSzama; i++)
+                    {
+                        sw.WriteLine(adat[i]);
+                        Console.WriteLine(adat[i]);
+                    }
+                }
+            }
+        }
+    }
+}
