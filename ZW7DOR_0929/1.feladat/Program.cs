ad9d062e22e853aa9bc1e8aaac291f04733c1e5d
PPatrik123 <78854607+PPatrik123@users.noreply.github.com>
2021-10-23 17:43:36 +0200

    Add files via upload

---
+++ ZW7DOR_0929/1.feladat/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ZW7DOR_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adatok száma: ");
+            int adat = Convert.ToInt32(Console.ReadLine());
+
+            using (FileStream fs = File.Open("Palko.txt", FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string[] adatok = sr.ReadLine().Split(',');
+                    if (adatok.Length < adat)
+                    {
+                        Console.WriteLine("A nincs ennyi adat.");
+                        return;
+                    }
+
+                    int[] szamok = new int[adat];
+                    int osszeg = 0;
+
+                    for (int i = 0; i < adat; i++)
+                    {
+                        szamok[i] = Convert.ToInt32(adatok[i]);
+                        osszeg += szamok[i];
+
+                        Console.WriteLine($"{i + 1}.adat = {szamok[i]}");
+                    }
+
+                    Console.WriteLine($"Összeg: {osszeg}");
+                }
+            }
+        }
+    }
+}
