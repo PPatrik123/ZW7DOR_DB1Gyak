ccc8a1efe8fa82ac1bb883d03a7de6546874e442
PPatrik123 <78854607+PPatrik123@users.noreply.github.com>
2021-10-23 18:03:24 +0200

    Add files via upload

---
+++ ZW7DOR_0929/3.feladat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ZW7DOR_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> szavak = new List<string>();
+
+            Console.Write("Adja meg a fájl nevét: ");
+            string fajlNev = Console.ReadLine();
+
+            string szo;
+            while ((szo = Console.ReadLine()) != "end")
+            {
+                szavak.Add(szo);
+            }
+
+            using (FileStream fs = File.Open(fajlNev, FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    foreach (string i in szavak)
+                    {
+                        sw.WriteLine(i.ToUpper());
+                    }
+                }
+            }
+        }
+    }
+}
